8419fdf1a9fdb3172020e27026bb143df298f47c
Nouman3307 <66219900+Nouman3307@users.noreply.github.com>
2020-06-10 13:21:31 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nouman from './nouman';
+function App() {
+  return (
+    <div >
+      <Nouman name="My first Code" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nouman.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Nouman(props)
+{
+  return (
+      <div>
+          <h1>{props.name}</h1>
+          <h1>My first code in React</h1>
+      </div>
+  )
+}
+export default Nouman;
