18843f5b2cc1d7d7c19ccc98ef02fd67e63e9733
Nouman3307 <66219900+Nouman3307@users.noreply.github.com>
2020-06-10 14:32:16 +0500

    Update nouman.js

--- src/nouman.js
+++ src/nouman.js
@@ -4,7 +4,7 @@ function Nouman(props)
   return (
       <div>
           <h1>{props.name}</h1>
-          <h1>My first code in React</h1>
+          <h1>My first code in React with bootcamp</h1>
       </div>
   )
 }
